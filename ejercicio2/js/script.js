bede95063fdb98a8e384324d8ae6c33b6e6c4595
Frank <100966778+FrankMen06@users.noreply.github.com>
2022-11-08 10:18:18 -0600

    Update script.js

--- ejercicio2/js/script.js
+++ ejercicio2/js/script.js
@@ -1,1 +1,26 @@
-let xd;
+function porcentaje(){
+
+    chesse = document.getElementById("chesse").value;
+    if(chesse == onclick){
+        contaChesee++;
+    }
+
+    flan = document.getElementById("flan").value;
+    if(flan == onclick){
+        contaFlan++;
+    }
+
+    helado = document.getElementById("helado").value;
+    if(helado == onclick){
+        contaHelado++;
+    }
+
+    lavaCake = document.getElementById("lavaCake").value;
+    if(lavaCake == onclick){
+        contaLava++;
+    }
+
+    total = contaChesee + contaFlan + contaHelado + contaLava;
+    
+
+}
