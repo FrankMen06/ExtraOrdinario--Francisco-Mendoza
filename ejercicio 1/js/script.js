ec80bc2a91e496b0747d9a87778b9893a4f1bf72
Frank <100966778+FrankMen06@users.noreply.github.com>
2022-11-08 10:13:33 -0600

    Update script.js

--- ejercicio 1/js/script.js
+++ ejercicio 1/js/script.js
@@ -2,15 +2,19 @@ function elevar(){
     numero = document.getElementById("numero").value;
     potencia =  document.getElementById("potencia").value;
 
+    
+
     for(i=0; i<=potencia; i++){
-        result = numero* numero;
-        resultado = document.getElementById("resultado");
-        resultado.innerHTML = "El resultado es: " +result;
+        result = (numero * numero)* i;
+      
     }
 
+    resultado = document.getElementById("resultado");
+    resultado.innerHTML = "El resultado es: " +result;  
     
     //"El resultado es: " + result;
 
     console.log(result);
+
     
 }
