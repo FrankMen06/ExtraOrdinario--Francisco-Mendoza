fe7ded89605a49fdbf376a76c8aa08d1410896ac
Frank <100966778+FrankMen06@users.noreply.github.com>
2022-11-08 09:34:28 -0600

    Create script.js

---
+++ ejercicio 1/js/script.js
@@ -0,0 +1,16 @@
+function elevar(){
+    numero = document.getElementById("numero").value;
+    potencia =  document.getElementById("potencia").value;
+
+    for(i=0; i<=potencia; i++){
+        result = numero* numero;
+        resultado = document.getElementById("resultado");
+        resultado.innerHTML = "El resultado es: " +result;
+    }
+
+    
+    //"El resultado es: " + result;
+
+    console.log(result);
+    
+}
